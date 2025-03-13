2e4168b52f201d9c38ef17e60ca8f364f19e9794
mayadahan77 <mayaschool555@gmail.com>
2025-03-13 13:58:37 +0200

    add users compoennts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
 import React, { useState } from 'react'
+import useUser from "../src/hooks/useUser";
+import { useEffect } from "react";
+
+
 
 import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from './components/RegistrationPage/LoginPage';
+import SignUpPage from './components/RegistrationPage/SignUpPage';
+import UserProfile from './components/UserProfile/UserProfile';
 
 function App() {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [user]);
 
   return (
-    <>
-      <Header />
+    <Router>
+      <div>
+        {isAuthenticated && (
+            <Header />
+        )}
+        <div>
+          <Routes>
+            {isAuthenticated ? (
+              <>
+                <Route path="/" />
+                <Route path="/profile" element={<UserProfile />} />
 
-    </>
-  )
+              </>
+            ) : (
+              <>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/SignUp" element={<SignUpPage />} />
+              </>
+            )}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
